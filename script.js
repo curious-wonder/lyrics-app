4ebd1789a66f005149def8829c98f0e31203b5ae
Athena Pascua <pascuaa@vcu.edu>
2025-12-02 16:58:17 -0500

    added comments and css changes

---
+++ script.js
@@ -0,0 +1,27 @@
+//selectors
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
+
+const apiURL = 'https://api.lyrics.ovh';
+
+//search by song or artist
+async function searchSongs(term) {
+    const res = await fetch(`${apiURL}/suggest/${term}`);
+    const data = await res.json();
+
+    console.log(data);
+}
+
+//event listeners
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (!searchTerm) {
+        alert('Please type in a search term');
+    } else {
+        searchSongs(searchTerm);
+    }
+});
+
